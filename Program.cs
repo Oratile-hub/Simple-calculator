bd747dbba8ec35380a59952b2889a9cb3a56b1bc
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-11 14:51:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            double num3 = num1 + num2;
+
+            Console.WriteLine(num1 + " + " + num2 + " = " + num3);
+
+
+
+            //freeze console
+            Console.ReadLine();
+
+
+        }
+    }
+}
